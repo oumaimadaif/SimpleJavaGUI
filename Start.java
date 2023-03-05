c666f5cf28166af5a3a1afa5a4fe588e24f592e1
oumaimadaif <125674737+oumaimadaif@users.noreply.github.com>
2023-03-05 15:37:34 +0100

    test class

--- Start.java
+++ Start.java
@@ -1,6 +1,6 @@
 
 public class Start {
-public static void main ( String[]args) {
+public static void main (String[] args) {
 	//print Hello World on the console 
 	System.out.println("Hello World");
 }
